f2a010a76206a59f3693678c01bb1323dc43b12b
Uczen 1 <teraz@wp.pl>
2025-10-21 16:01:18 +0200

    Dodano pętlę while

--- 23_Operatory_porownania/Przykład 3_18/script.js
+++ 23_Operatory_porownania/Przykład 3_18/script.js
@@ -1,10 +1,11 @@
 //3.18
 let a = 10;
-let b='10';
-if(a==b){
+let b ='10';
+if(a=b){
     document.write("Liczby są równe.");
 }
 else
 {
     document.write("Liczby nie są równe.");
 }
+document.write(a + " " + typeof a)
