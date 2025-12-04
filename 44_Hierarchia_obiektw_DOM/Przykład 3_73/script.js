6123664555179ca1308040fdf8415aca478593fc
Uczen 1 <teraz@wp.pl>
2025-12-04 11:20:06 +0100

    metody czasu

--- 44_Hierarchia_obiektw_DOM/Przykład 3_73/script.js
+++ 44_Hierarchia_obiektw_DOM/Przykład 3_73/script.js
@@ -1,2 +1,12 @@
+function showtime() {
+    let data_n= new Date();
+    let godz =data_n.getHours();
+    let min= data_n.getMinutes();
+    let sek= data_n.getSeconds();
+    let czas ="" +godz;
+    czas+=((min <10)?":0": ":") + min;
+    czas+=((sek <10)?":0": ":") + sek;
+console.log(czas);
+}
 //Przykład 3.73
 setTimeout("showtime()",1000);
