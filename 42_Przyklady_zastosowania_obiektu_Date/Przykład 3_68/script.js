7e9aafdf53d61af91776b0f38d747d729e16c9c9
Uczen 1 <teraz@wp.pl>
2025-12-10 08:43:52 +0100

    przykłady-do-daty

--- 42_Przyklady_zastosowania_obiektu_Date/Przykład 3_68/script.js
+++ 42_Przyklady_zastosowania_obiektu_Date/Przykład 3_68/script.js
@@ -1,3 +1,10 @@
 // przykład 3.68
 let data = new Date(2019, 2, 27);
 console.log(data);
+console.log(data.toString()); //zwraca datę, czas oraz informacje o strefie czasowej w języku angielskim
+console.log(data.toLocaleDateString()); //zwraca datę i czas dla bieżących ustawień regionalnych
+console.log(data.toUTCString()); //zwraca datę, czas oraz informacje o strefie czasowej dla formatu UTC (Universal Coordinated Time)
+console.log(data.toDateString()); //zwraca tylko datę w języku angielskim
+console.log(data.toLocaleDateString()); //zwraca tylko datę dla bieżących ustawień regionalnych
+console.log(data.toTimeString()); //zwraca tylko czas w języku angielskim
+console.log(data.toLocaleTimeString()); //zwraca tylko czas dla bieżących ustawień regionalnych
