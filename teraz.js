5e28002a0a7f0fcf4e37d470c3c2ab602131705a
Uczen 1 <teraz@wp.pl>
2026-03-02 12:28:14 +0100

    Uzupełnienie funkcji strzałkowej.

--- teraz.js
+++
@@ -1,9 +0,0 @@
-// Sposoby na uzyskanie NaN
-console.log(Math.sqrt(-1));
-console.log(1 + NaN);
-console.log(undefined + undefined);
-console.log('foo' / 3);
-/// - To jest dopisek
-///// - dopisek 2
-///// - fork 123
-
